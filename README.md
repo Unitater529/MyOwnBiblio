5c51018b637fa550ae014e03d7b0b56c97756d83
Unitater529 <78939044+Unitater529@users.noreply.github.com>
2021-02-25 15:48:02 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyOwnBiblio
+# collisionAlgorithm
+Algorithm to detect collision between two objects
---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+
+  fixedRect1 = createSprite(200, 100, 50, 50);
+  fixedRect1.shapeColor = "green";
+  
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,fixedRect)){
+    movingRect.shapeColor = "pink";
+    fixedRect.shapeColor = "pink";
+  }
+  
+  else if(isTouching(movingRect,fixedRect1)){
+    
+    movingRect.shapeColor = "purple";
+  
+    fixedRect.shapeColor = "purple"
+  }
+  else if(fixedRect1){
+    movingRect.shapeColor = "blue";
+  
+    fixedRect.shapeColor = "blue"
+  }
+  else if (isTouching(fixedRect)){
+    movingRect.shapeColor = "yellow";
+  
+   fixedRect.shapeColor = "yellow"
+  }
+  
+
+  
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+
+
+
